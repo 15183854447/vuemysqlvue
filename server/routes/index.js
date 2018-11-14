06e4d2e2356d68a7db79b92010a99fd87b0a1948
15183854447 <33994091+15183854447@users.noreply.github.com>
2018-11-14 10:20:17 +0800

    Signed-off-by: 15183854447 <33994091+15183854447@users.noreply.github.com> 1.修改

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 var db = require('../db')
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    db.query('select * from users', [], function (results, fields) {
-        console.log(results)
-    });
-    res.render('index', {title: 'Express'});
-});
+// router.get('/', function (req, res, next) {
+//     // db.query('select * from users', [], function (results, fields) {
+//     //     console.log(results)
+//     // });
+//     // res.render('index', {title: 'Express'});
+// });
 router.get('/list.html', function (req, res, next) {
     db.query('select * from users', [], function (results, fields) {
         res.send(results)
