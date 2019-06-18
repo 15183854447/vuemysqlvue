445744397026e6568ceac10eed46d724c2b8a678
15183854447 <33994091+15183854447@users.noreply.github.com>
2019-06-18 17:03:34 +0800

    swagger 集成
    Signed-off-by: 15183854447 <33994091+15183854447@users.noreply.github.com>

--- server/app.js
+++ server/app.js
@@ -5,10 +5,44 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+var swaggerUi = require('swagger-ui-express');
+var swaggerJSDoc = require('swagger-jsdoc');
+
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+//配置 swaggerJSDoc
+
+const options = {
+    definition: {
+        // swagger 采用的 openapi 版本 不用改
+        openapi: '3.0.0',
+        // swagger 页面基本信息 自由发挥
+        info: {
+            title: '豆瓣电影api',
+            description: '更多详情，查看      [https://github.com/15183854447/vuemysqlvue](https://github.com/15183854447/vuemysqlvue)',
+            version: '1.0.0',
+        },
+        basePath: '',
+    },
+    // 重点，指定 swagger-jsdoc 去哪个路由下收集 swagger 注释
+    apis: [path.join(__dirname, '/routes/*.js')]
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+// 开放 swagger 相关接口，
+app.get('/swagger.json', function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+});
+
+// 使用 swaggerSpec 生成 swagger 文档页面，并开放在指定路由
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 //设置跨域访问
 app.all('*', function (req, res, next) {
@@ -23,8 +57,11 @@ app.all('*', function (req, res, next) {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -52,12 +89,14 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
+
+
 app.listen('9528', function (err) {
     if (err) {
-        console.log('服务器启动失败')
+        console.log('9528端口、服务器启动失败');
         return;
     }
-    console.log('服务器启动成功')
-})
+    console.log('9528端口、服务器启动成功')
+});
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,17 +1,71 @@
 var express = require('express');
 var router = express.Router();
 
-var db = require('../db')
-/* GET home page. */
-// router.get('/', function (req, res, next) {
-//     // db.query('select * from users', [], function (results, fields) {
-//     //     console.log(results)
-//     // });
-//     // res.render('index', {title: 'Express'});
-// });
-router.get('/list.html', function (req, res, next) {
+var db = require('../db');
+
+
+/**
+ * @swagger
+ * /list.html/{id}:
+ *   get:
+ *     tags:
+ *       - 添加
+ *     description: 添加用户
+ *     summary: 查询用户列表中名字带有s的
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: 用户id
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: Successfully created
+ */
+router.get('/list.html/:id', function (req, res, next) {
     db.query('select * from users', [], function (results, fields) {
         res.send(results)
     })
 });
+/**
+ * @swagger
+ * /jz.html:
+ *   post:
+ *     tags:
+ *       - 用户
+ *     summary: 查询用户列表中名字带有s的
+ *     description: 所有用户信息或者用户的集合信息
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: puppy
+ *         description: object
+ *         in: body
+ *         required: true
+ *         type: string
+ *         schema:
+ *           type: array
+ *           items:
+ *             required:
+ *               - username
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               username:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: 【成功】 返回 world
+ */
+router.post('/jz.html', function (req, res, next) {
+    var sql = "select * from users WHERE username like ?";
+    // sql += " where var_name like ‘%"+data.varName+"%‘";
+    db.query(sql, ['%s%'], function (results, fields) {
+        res.send(results)
+    })
+});
+
+
 module.exports = router;
