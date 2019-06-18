77ef0784ce76cd560981369abd0c722fe9d7b7fa
15183854447 <33994091+15183854447@users.noreply.github.com>
2019-06-18 21:30:11 +0800

    swagger 参数的注释的编写

--- server/routes/users.js
+++ server/routes/users.js
@@ -31,15 +31,91 @@ router.get('/apps', async (req, res, next) => {
  *   get:
  *     tags:
  *     - 用户
- *     summary: 用户登录
+ *     summary: 用户查询
  */
-router.get('/info', function (req, res, next) {
-    console.log(req.query)
-    console.log(req.body)
-    res.json({'name':"张三","age":28})
+router.get('/info/:id', function (req, res, next) {
+    console.log(req.params.id);//获取参数
+    res.json({'name': "张三", "age": 28})
     // db.query('select * from users', [], function (results, fields) {
     //     res.send(results)
     // })
 });
-
+/**
+ *@swagger
+ *  /users/login:
+ *   get:
+ *    tags:
+ *    - 用户
+ *    summary: 用户登录
+ */
+router.get('/login', function (req, res, next) {
+    console.log(req.query);
+    res.send(req.query)
+});
+/**
+ *@swagger
+ *  /users/login/a:
+ *   post:
+ *    tags:
+ *    - 用户
+ *    summary: 用户登录
+ *    description: 用户登录的接口
+ *    parameters:
+ *      - name: body
+ *        in: body
+ *        require: true
+ *        schema:
+ *          type: object
+ *          properties:
+ *            id:
+ *              type: integer
+ *            username:
+ *              type: string
+ *    responses:
+ *       404:
+ *         description: 失败
+ */
+router.post('/login/a', function (req, res, next) {
+    console.log(req.body);
+    res.send(req.body)
+});
+/**
+ *@swagger
+ *  /users/zhangsan/a:
+ *   post:
+ *    tags:
+ *    - 用户
+ *    summary: 用户登录
+ *    description: 用户登录的接口
+ *    consumes:
+ *      - application/json
+ *      - application/xml
+ *    parameters:
+ *      - name: body
+ *        in: body
+ *        description: 传递参数说明
+ *        require: true
+ *        schema:
+ *          type: object
+ *          properties:
+ *            id:
+ *              type: integer
+ *            username:
+ *              type: string
+ *            store:
+ *              type: array
+ *              items:
+ *                properties:
+ *                  first:
+ *                    type: string
+ *                  sencond:
+ *                    type: string
+ *    responses:
+ *       404:
+ *         description: 失败
+ */
+router.post('/zhangsan/a', function (req, res, next) {
+    console.log(req.body);
+    res.send(req.body)
+});
 module.exports = router;
